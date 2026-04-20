09aa9de79841cdf72da4813c78089304ca886668
Mikhail Zizganov <mzizganov@yandex.ru>
2026-04-20 22:23:05 +0300

    Добавлен контроллер product_detail, обновлен home с ORM-запросом

--- catalog/views.py
+++ catalog/views.py
@@ -1,20 +1,20 @@
-from django.shortcuts import render
-from django.contrib import messages  # 👈 добавить эту строку
-
+from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+from .models import Product
 
 def home(request):
-    return render(request, 'catalog/home.html')
-
+    products = Product.objects.all()  # 👈 получаем все товары
+    return render(request, 'catalog/home.html', {'products': products})
 
 def contacts(request):
-    if request.method == 'POST':  # 👈 проверяем, отправили ли форму
+    if request.method == 'POST':
         name = request.POST.get('name')
         phone = request.POST.get('phone')
         message = request.POST.get('message')
-
-        # Пока просто показываем сообщение (отправка email по желанию)
         messages.success(request, f'Спасибо, {name}! Ваше сообщение отправлено.')
-
         return render(request, 'catalog/contacts.html')
-
     return render(request, 'catalog/contacts.html')
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'catalog/product_detail.html', {'product': product})
