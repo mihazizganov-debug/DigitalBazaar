81bb046c793cf625c8976c81816cf2a89ac7fad9
Mikhail Zizganov <mzizganov@yandex.ru>
2026-04-27 18:08:06 +0300

    fix: исправлен admin.py (PEP8)

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
+
 from .models import Category, Product
+
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name')
-    search_fields = ('name',)
+    list_display = ("id", "name")
+    search_fields = ("name",)
+
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'price', 'category')
-    list_filter = ('category',)
-    search_fields = ('name', 'description')
+    list_display = ("id", "name", "price", "category")
+    list_filter = ("category",)
+    search_fields = ("name", "description")
