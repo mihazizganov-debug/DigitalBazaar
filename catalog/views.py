397f466d01063a44764aedfc0eaf6a36214d1cd5
Mikhail Zizganov <mzizganov@yandex.ru>
2026-05-03 11:50:13 +0300

    feat: добавлены контроллеры CreateView, UpdateView, DeleteView

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,10 @@
 from django.contrib import messages
 from django.shortcuts import render
-from django.views.generic import DetailView, ListView, TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import (CreateView, DeleteView, DetailView, ListView,
+                                  TemplateView, UpdateView)
 
+from .forms import ProductForm
 from .models import Product
 
 
@@ -21,11 +24,32 @@ class ContactsView(TemplateView):
     template_name = "catalog/contacts.html"
 
     def post(self, request, *args, **kwargs):
-        name = request.POST.get('name', 'Гость')
-        phone = request.POST.get('phone', 'не указан')
-        message = request.POST.get('message', 'пустое сообщение')
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
         messages.success(
             request,
-            f'Спасибо, {name}! Сообщение "{message}" отправлено. Мы позвоним по номеру {phone}.'
+            f'Спасибо, {name}! Телефон: {phone}. Сообщение: {message}'
         )
         return render(request, self.template_name)
+
+
+# CRUD для продуктов
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    template_name = "catalog/product_form.html"
+    success_url = reverse_lazy("catalog:home")
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductForm
+    template_name = "catalog/product_form.html"
+    success_url = reverse_lazy("catalog:home")
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = "catalog/product_confirm_delete.html"
+    success_url = reverse_lazy("catalog:home")
