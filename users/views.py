64726eea81b0a1633e702144a80dea16701808db
Mikhail Zizganov <mzizganov@yandex.ru>
2026-05-27 20:58:24 +0300

    fix: исправлены ошибки PEP8 в users/views.py

--- users/views.py
+++ users/views.py
@@ -1,8 +1,8 @@
 from django.conf import settings
+from django.contrib.auth.views import LoginView
 from django.core.mail import send_mail
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
-from django.contrib.auth.views import LoginView
 
 from .forms import UserLoginForm, UserRegisterForm
 from .models import User
