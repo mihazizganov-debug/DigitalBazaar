92a45feecba2f78479553717b3ad70c7d1ff5191
Mikhail Zizganov <mzizganov@yandex.ru>
2026-05-27 20:24:24 +0300

    feat: добавлены настройки кеширования Redis

--- config/settings.py
+++ config/settings.py
@@ -130,3 +130,10 @@ AUTH_USER_MODEL = "users.User"
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 LOGIN_URL = "/users/login/"
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1',
+    }
+}
