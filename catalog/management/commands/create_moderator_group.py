2a24e861d13c7a40fe093d45fb3cdfde90a8f117
Mikhail Zizganov <mzizganov@yandex.ru>
2026-05-27 20:57:39 +0300

    feat: команда create_moderator_group для создания группы модераторов

--- catalog/management/commands/create_moderator_group.py
+++ catalog/management/commands/create_moderator_group.py
@@ -1,6 +1,7 @@
-from django.core.management.base import BaseCommand
 from django.contrib.auth.models import Group, Permission
 from django.contrib.contenttypes.models import ContentType
+from django.core.management.base import BaseCommand
+
 from catalog.models import Product
 
 
